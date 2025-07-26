ca69d688c6188c62007512a7b89198107af7385b
Uchechi Jethro Chizuru <113516545+Jay-Tiroh@users.noreply.github.com>
2025-07-26 01:32:00 +0100

    Update MyProject.tsx
    Updated the space exploration link

--- src/components/MyProject.tsx
+++ src/components/MyProject.tsx
@@ -31,7 +31,7 @@ const MyProjects = () => {
         "Space exploration site with smooth navigation, visuals, and mobile responsiveness.",
       stack: ["react", "js", "tailwind", "css"],
       image: "/assets/images/space.png",
-      link: "https://github.com/Jay-Tiroh/firebase-app",
+      link: "https://firebase-app-y84x.vercel.app/",
     },
 
     {
